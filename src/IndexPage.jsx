cb30966a365bcbbf95d092515227dda2fa569653
Elia <EliaRenov04@gmail.com>
2023-04-22 10:59:27 +0300

    Upload image assets to style index page

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -1,21 +1,27 @@
 import './IndexPage.css'
 import AesopPic from './assets/aesop-with-background.png'
 import Scroll from './assets/scroll.png'
+import Warrior from './assets/warrior.png'
+import Warrior2 from './assets/warrior2.png'
+import Dice from './assets/dice.png'
 
 export default function IndexPage() {
 
     return (
-        <>
-
-            <div className="container main">
+        <div className="container">
+            <img src={Warrior} height="580px" className="backwards" />
+            <div className="nav-container main">
                 <h1 className="header main">משלי איזופוס</h1>
                 <img src={AesopPic} alt="דיוקן של איזופוס" className="aesop-pic main" />
                 <section className="buttons main">
                     <a href="" className="random main">
-                    <img src={Scroll} height="30px" width="30px" /> משל אקראי 
+                    <img src={Dice}width="120px" />
+                     משל אקראי 
+                     <img src={Dice}width="120px" />
                     </a>
                     <a href="" className="collection main">
-                        אוסף משלים
+                    <img src={Scroll}width="120px" />    אוסף משלים
+                    <img src={Scroll}width="120px" />  
                     </a>
                     <a href="" className="about main">
                         אודות
@@ -29,7 +35,7 @@ export default function IndexPage() {
 
             </div>
 
-
-        </>
+            <img src={Warrior2} height="550px" />
+        </div>
     )
 }
