4a264ee43b949bcef008cd11346c2d57d5864092
Elia <EliaRenov04@gmail.com>
2023-04-24 14:27:12 +0300

    Fix asset management

--- src/functions.jsx
+++ src/functions.jsx
@@ -1,6 +1,20 @@
 import { mashals, mashalIndices } from "./mashalsDB";
 import React, { useState } from 'react'
 import * as mashal from './MashalPage.module.css'
+import Bear from './assets/animals/bear.png'
+import Bunny from './assets/animals/bunny.png'
+import Duck from './assets/animals/duck.png'
+import Eagle from './assets/animals/eagle.png'
+import Fox from './assets/animals/fox.png'
+import Frog from './assets/animals/frog.png'
+import Goat from './assets/animals/goat.png'
+import Horse from './assets/animals/horse.png'
+import Lion from './assets/animals/lion.png'
+import Scorpion from './assets/animals/scorpion.png'
+import Sheep from './assets/animals/sheep.png'
+import Snake from './assets/animals/snake.png'
+import Turtle from './assets/animals/turtle.png'
+import Wolf from './assets/animals/wolf.png'
 
 export function clear() {
     while (document.querySelector("root").firstChild) {
@@ -10,16 +24,14 @@ export function clear() {
 
 export function RandomImg() {
     const randomNum = Math.floor(Math.random() * 14)
-    const animals = ['bear', 'bunny', 'duck', 'eagle', 'fox', 'frog', 'goat', 'horse', 'lion', 'scorpion', 'sheep', 'snake', 'turtle', 'wolf']
+    const animals = [Bear, Bunny, Duck, Eagle, Fox, Frog, Goat, Horse, Lion, Scorpion, Sheep, Snake, Turtle, Wolf]
     return (
         <img
-        src={`src/assets/animals/${animals[randomNum]}.png`}
+        src={animals[randomNum]}
         alt={`${animals[randomNum]} drawing`} className={mashal.animal}
         />
         )
 }
-
-
 
 export function getRandomMashal() {
     const mashalsKeys = Object.keys(mashals);
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     hmr: {
         host: 'localhost',
     },
+  base: '/mashal-react'
 }
 })
 
